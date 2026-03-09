a4ce3857020c38179abcbc1e5d18e7297fc3cca3
lucasscianna <you@example.com>
2026-03-09 10:32:08 +0000

    ajout de bycrypt dans --init.py-- pour installer l'outil au coeur de l'app

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask_restx import Api
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     
     # On ajoute cette ligne pour charger les réglages du fichier config.py
     app.config.from_object(config_class)
+    bcrypt.init_app(app)
     
     from app.api.v1 import blueprint as api_v1
     app.register_blueprint(api_v1)
-    
+
     return app
