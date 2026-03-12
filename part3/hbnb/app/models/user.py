0febea700174084cabb8b3261109176026db51a7
Ilnnn <camoreze@gmail.com>
2026-03-12 10:01:58 +0000

    beggining to add user entity

--- part3/hbnb/app/models/user.py
+++ part3/hbnb/app/models/user.py
@@ -1,35 +1,31 @@
 import re
+from sqlalchemy.orm import validates
 from app.models.base_model import BaseModel
-from app import bcrypt
+from app import db, bcrypt
 
 class User(BaseModel):
     """Represents a user in the system."""
-    _emails = set()
+    __tablename__ = 'users'
+    
+    first_name = db.Column
+    last_name = db.Column
+    email = db.Column
+    password = db.Column
 
-    def __init__(self, first_name, last_name, email, password, is_admin=False):
-        """Initializes a user with name, email validation, and password hashing."""
-        super().__init__()
-        self.first_name = self.validate_name(first_name, "First name")
-        self.last_name = self.validate_name(last_name, "Last name")
-        self.email = self.validate_email(email)
-        self.is_admin = is_admin
-        # Hash the password immediately upon creation
-        self.hash_password(password)
-
-    def validate_name(self, value, field_name):
+    @validates('first_name', 'last_name')
+    def validate_name(self, key, value):
         """Ensures the name is not empty and does not exceed 50 characters."""
         if not value or len(value) > 50:
+            field_name = "First name" if key == "first_name" else "Last name"
             raise ValueError(f"{field_name} is required (max 50 characters).")
         return value
     
-    def validate_email(self, email):
+    @validates('email')
+    def validate_email(self, key, email):
         """Validates the email format and ensures its uniqueness."""
         email_regex = r"^[\w\.-]+@[\w\.-]+\.\w+$"
         if not re.match(email_regex, email):
             raise ValueError("Invalid email format.")
-        if email in User._emails:
-            raise ValueError("Email must be unique.")
-        User._emails.add(email)
         return email
 
     def hash_password(self, password):
